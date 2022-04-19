c9ddfcd6980d0ba9c4b630e30dde4de541d0bb9c
AvinashSutariya <avinash.s@simformsolutions.com>
2022-04-19 17:33:16 +0530

    design change

--- BidSignalR/Startup.cs
+++ BidSignalR/Startup.cs
@@ -68,7 +68,7 @@ namespace Playground
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Lot}/{action=Index}/{id?}");
             });
         }
     }
